e410caf619798583c182428cd1dcb5ecde32bc3f
catsandygo-design <catsandy.go@gmail.com>
2026-07-22 16:31:40 -0300

    Liberar MIME de documentos compactados

---
+++ supabase/migrations/20260722000300_allow_compressed_documents.sql
@@ -0,0 +1,23 @@
+
+update storage.buckets
+set allowed_mime_types = array[
+  'image/jpeg',
+  'image/png',
+  'image/gif',
+  'image/bmp',
+  'application/pdf',
+  'application/msword',
+  'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+  'application/vnd.ms-excel',
+  'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+  'application/zip',
+  'application/x-zip-compressed',
+  'application/gzip',
+  'application/x-gzip',
+  'application/vnd.rar',
+  'application/x-rar-compressed',
+  'application/x-7z-compressed',
+  'application/octet-stream'
+]
+where id = 'reservation-documents';
+
